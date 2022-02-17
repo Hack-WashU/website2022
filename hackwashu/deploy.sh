1858d069cc95083a6fdf32fd4cc1566df0d05dfe
Seojin Kim <skim27@wustl.edu>
2022-02-17 16:25:35 -0600

    attempt to deploy

---
+++ hackwashu/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+echo 'www.hackwashu.io' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:SeojinK1m/hello-world.git master:gh-pages
+cd -
