36e8798ecb480acc60f1f0f2f629573e821c1b93
Seojin Kim <skim27@wustl.edu>
2022-06-24 21:32:24 -0700

    redo tracks

--- src/components/Tracks.vue
+++ src/components/Tracks.vue
@@ -1,27 +1,44 @@
 <template>
 <div id="tracksAndDivisions">
+    <h1>Our <span>Tracks</span></h1>
     <div class="tracks">
-        <h1>Tracks</h1>
-        <h2>Sustainability</h2>
-        <h3>Sustainable practices support ecological, human, and economic health and vitality. The sustainability track is for hackers who aim to improve environmental sustainability through the use of technology.</h3>
-        <h2>Security and Privacy</h2>
-        <h3>The safeguarding of data and user identity in various platforms is an increasingly pressing issue in our data driven world. The Security and Privacy track is for hackers who aim to improve how we protect our data.</h3>
+        <TraDirContainer
+            title="Sustainability"
+            text="Sustainable practices support ecological, human, and economic health and vitality. The sustainability track is for hackers who aim to improve environmental sustainability through the use of technology."
+        />
+        <TraDirContainer 
+            title="Security and Privacy"
+            text="The safeguarding of data and user identity in various platforms is an increasingly pressing issue in our data driven world. The Security and Privacy track is for hackers who aim to improve how we protect our data."
+        />
+        <TraDirContainer 
+            title="Interactive Media"
+            text="From terminal windows in the 60s to full blown virtual reality UIs in modern times, the way we interact with technology has evolved quickly. The Interactive Media track is for hackers who wish to push the limits of how we interact with technology."
+        />
+        <TraDirContainer 
+            title="Another Track"
+            text="lorem ipsum."
+        />
     </div>
+    <h1>Our <span>Divisions</span></h1>
     <div class="divisions">
-        <h1>Divisions</h1>
-        <h2>Master</h2>
-        <h3>The master division is for more experienced hackers who are comfortable with having full creative control of their projects. We will have exciting speakers and experienced mentors to help take your project to the next level.</h3>
-        <h2>Emerging</h2>
-        <h3>The emerging division is for underclassmen who may not be as experienced in building their own projects. We will have mentors and volunteers to help guide you through building your projects.</h3>
+        <TraDirContainer 
+            title="Master"
+            text="The master division is for more experienced hackers who are comfortable with having full creative control of their projects. We will have exciting speakers and experienced mentors to help take your project to the next level."
+        />
+        <TraDirContainer 
+            title="Emerging"
+            text="The emerging division is for underclassmen who may not be as experienced in building their own projects. We will have mentors and volunteers to help guide you through building your projects."
+        />
     </div>
 </div>
 </template>
 
 <script>
-
+import TraDirContainer from './TraDirContainer.vue'
 export default {
   name: 'Tracks',
   components: {
+      TraDirContainer
   }
 }
 </script>
@@ -31,23 +48,28 @@ export default {
  #tracksAndDivisions {
      margin-top: 5vh;
      max-width: 900px;
-     min-width: 300px;
+     min-width: 350px;
      min-height: 90vh;
      display: flex;
      flex-direction: column;
      justify-content: center;
      margin-left: auto;
      margin-right: auto;
-     text-align: left;
+     text-align: center;
 }
 
-.tracks {
-    margin-bottom: 20px;
+.tracks, .divisions {
+    margin-bottom: 40px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
 }
 
 h1 {
-    color: var(--orange);
+    color: white;
     margin: 7px;
+    font-size: 3.5em;
 }
 
 h2, h3 {
@@ -55,10 +77,18 @@ h2, h3 {
     color: white;
 }
 
+span {
+    color: rgba(109, 223, 144, 1);
+}
+
 @media only screen and (max-width: 1000px) {
 #tracksAndDivisions {
     width: 80%;
     min-height: 70vh;
+}
+
+h1 {
+    font-size: 2em;
 }
 }
 </style>
