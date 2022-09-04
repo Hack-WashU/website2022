7052d1b79b500b6c2855b52d0a3ba6d439ffa5c0
bradley <bradley@bradleys-MacBook-Pro.local>
2022-09-04 03:09:01 -0500

    added hover behavior to apply button

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -3,12 +3,11 @@
         <img id="title" src="../assets/hackwashu-ab.svg" />
         <div id="info">
             <h1 id="date">OCTOBER 14 - 16, 2022</h1>
-            <h2 id="interested">applications open now!!!</h2>
-            <button onclick="window.location.href='https://register.hackwashu.com';" id="subscribe">
-                <h2 id="soon">REGISTER NOW</h2>
+            <h2 id="interested">applications open now!!! click below to register!!!</h2>
+            <button onclick="window.open('https://register.hackwashu.com');" class="subscribe">
+                <h2 id="soon">APPLY NOW</h2>
             </button>
         </div>
-        
     </div>
 </template>
 
@@ -26,20 +25,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-#subscribe {
+.subscribe {
     clear:both; 
-    background-color: var(--green); 
-    border: none;
+    background-color: rgb(50, 52, 62);
+    border: 1px solid var(--white);
+    border-radius: 12px;
     box-shadow: 2;
-    color: var(--background); 
+    color: var(--white); 
     cursor: pointer; 
     font-weight: 600; 
 
-    margin: 2vh;
+    margin: 3vh;
     width: 200px;
     height: 80px;
 
     text-align: center;
+} 
+.subscribe:hover {
+    border: 2px solid rgba(149, 255, 182, 1);
+    background-color: rgba(49, 107, 67, 1);
+
 } 
 
 #title {
@@ -100,9 +105,9 @@ h2 {
         font-size: 1.2em;
         margin-top: 2vh;
     }
-    #subscribe {
+    .subscribe {
         width: 120px;
-        height: 48px;
+        height: 55px;
     }
 }
 
